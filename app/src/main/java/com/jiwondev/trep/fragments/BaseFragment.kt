32594aa72ee69dc12209069aa90faa0136871050
jiwon2724 <stopone3119@naver.com>
2022-12-28 18:51:19 +0900

    BottomNavigationView + Navigation 설정 Fragment 생성, 각 bottom menu의 아이템

---
+++ app/src/main/java/com/jiwondev/trep/fragments/BaseFragment.kt
@@ -0,0 +1,18 @@
+package com.jiwondev.trep.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jiwondev.trep.R
+
+class BaseFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_base, container, false)
+    }
+}
