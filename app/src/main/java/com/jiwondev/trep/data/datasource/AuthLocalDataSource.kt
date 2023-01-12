44ad6b7c4daa860448c8fe7335bf79ec90f63d4f
jiwon2724 <stopone3119@naver.com>
2023-01-12 11:47:03 +0900

    activity_sign_up ScrollView 추가

--- app/src/main/java/com/jiwondev/trep/data/datasource/AuthLocalDataSource.kt
+++ app/src/main/java/com/jiwondev/trep/data/datasource/AuthLocalDataSource.kt
@@ -6,6 +6,7 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import com.jiwondev.trep.model.preference.UserPreferences
 import com.jiwondev.trep.resource.PreferencesKeys
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
